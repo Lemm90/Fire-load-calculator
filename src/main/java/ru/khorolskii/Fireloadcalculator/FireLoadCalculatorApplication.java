fb800c3946ad1fc066bd02029f8921ab4a7270a6
Peter <khorolskii@gmail.com>
2023-09-13 00:41:58 +0300

    инициализация проекта

---
+++ src/main/java/ru/khorolskii/Fireloadcalculator/FireLoadCalculatorApplication.java
@@ -0,0 +1,13 @@
+package ru.khorolskii.Fireloadcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireLoadCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FireLoadCalculatorApplication.class, args);
+	}
+
+}
